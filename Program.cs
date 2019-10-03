1289748ef6f5519222b7b0cb37402d0e8e6fc341
Константин Дьяченко <konard@me.com>
2019-10-04 00:57:28 +0300

    Refactoring.

--- Program.cs
+++ Program.cs
@@ -10,115 +10,50 @@ using ICSharpCode.Decompiler.TypeSystem;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
-namespace CecilExample
+namespace CSharpRecompileExample
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            GetAllReferencesPaths();
-
-            ICSharpCode.Decompiler.CSharp.Syntax.SyntaxTree syntaxTree = GetSyntaxTreeOfThisClass();
-
-            //var astBuilder = new AstBuilder
-
-            // AstBuilder.AddMethod()
-
-            //ICSharpCode.Decompiler.
-
-            var code = syntaxTree;
-
-            Console.WriteLine(syntaxTree);
-
-            var roslynTree = CSharpSyntaxTree.ParseText(code.ToString());
-
-            var dotnetCoreDirectory = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory(); // Path.GetDirectoryName(typeof(System.Object).GetTypeInfo().Assembly.Location);
-
-            CSharpCompilation compilation = CSharpCompilation.Create(
-                "assemblyName",
-                new[] { roslynTree },
-                //GetAllReferencesPaths().Select(x => MetadataReference.CreateFromFile(x)).ToArray(),
-                new[]
-                {
-                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
-                    MetadataReference.CreateFromFile(typeof(Console).Assembly.Location),
-                    MetadataReference.CreateFromFile(typeof(MetadataReference).Assembly.Location),
-                    MetadataReference.CreateFromFile(Path.Combine(dotnetCoreDirectory, "mscorlib.dll")),
-                    MetadataReference.CreateFromFile(Path.Combine(dotnetCoreDirectory, "netstandard.dll")),
-                    MetadataReference.CreateFromFile(Path.Combine(dotnetCoreDirectory, "System.Collections.Immutable.dll")),
-                    MetadataReference.CreateFromFile(Path.Combine(dotnetCoreDirectory, "System.Runtime.dll")),
-                    MetadataReference.CreateFromFile(Path.Combine(dotnetCoreDirectory, "System.Runtime.Extensions.dll")),
-                    MetadataReference.CreateFromFile(Path.Combine(dotnetCoreDirectory, "System.Reflection.Metadata.dll")),
-                    MetadataReference.CreateFromFile(Path.Combine(dotnetCoreDirectory, "System.Private.Uri.dll")),
-                    MetadataReference.CreateFromFile(Path.Combine(dotnetCoreDirectory, "System.Linq.dll")),
-                    MetadataReference.CreateFromFile(Path.Combine(dotnetCoreDirectory, "System.Runtime.InteropServices.dll")),
-                    MetadataReference.CreateFromFile(typeof(ICSharpCode.Decompiler.CSharp.Syntax.SyntaxTree).Assembly.Location),
-                    MetadataReference.CreateFromFile(typeof(CSharpSyntaxTree).Assembly.Location),
-                },
-                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
+            var typeName = typeof(Program).FullName;
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var syntaxTree = executingAssembly.Decompile(typeName);
+            var code = syntaxTree.ToString();
+            Console.WriteLine(code);
             var fileName = "LibraryName.dll";
-            var emitResult = compilation.Emit(fileName);
+            var emitResult = Compile(code, fileName);
             if (emitResult.Success)
             {
-                var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(Path.GetFullPath(fileName));
-
-                Console.WriteLine(assembly.GetType(GetThisTypeName()).GetMethod("Add").Invoke(null, null));
+                var compiledAssembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(Path.GetFullPath(fileName));
+                Console.WriteLine(compiledAssembly.GetType(typeName).GetMethod("Add").Invoke(null, null));
             }
         }
 
-        public static int Add()
+        public static int Add() => 42;
+
+        private static Microsoft.CodeAnalysis.Emit.EmitResult Compile(string code, string fileName)
         {
-            return 42;
+            var roslynTree = CSharpSyntaxTree.ParseText(code);
+            var dotnetCoreDirectory = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory(); // Path.GetDirectoryName(typeof(System.Object).GetTypeInfo().Assembly.Location);
+            var references = Assembly.GetExecutingAssembly().GetAllReferencesPaths().ToList();
+            references.Add(Path.Combine(dotnetCoreDirectory, "mscorlib.dll"));
+            references.Add(Path.Combine(dotnetCoreDirectory, "netstandard.dll"));
+            references.Add(Path.Combine(dotnetCoreDirectory, "System.Collections.Immutable.dll"));
+            references.Add(Path.Combine(dotnetCoreDirectory, "System.Linq.dll"));
+            references.Add(Path.Combine(dotnetCoreDirectory, "System.Private.Uri.dll"));
+            references.Add(Path.Combine(dotnetCoreDirectory, "System.Private.CoreLib.dll"));
+            references.Add(Path.Combine(dotnetCoreDirectory, "System.Runtime.dll"));
+            references.Add(Path.Combine(dotnetCoreDirectory, "System.Runtime.Extensions.dll"));
+            references.Add(Path.Combine(dotnetCoreDirectory, "System.Runtime.InteropServices.dll"));
+            references.Add(Path.Combine(dotnetCoreDirectory, "System.Reflection.Metadata.dll"));
+            var metadataReferences = references.Select(x => MetadataReference.CreateFromFile(x)).ToArray();
+            var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+            CSharpCompilation compilation = CSharpCompilation.Create("assemblyName", new[] { roslynTree }, metadataReferences, options);
+            return compilation.Emit(fileName);
         }
 
-        private static ICSharpCode.Decompiler.CSharp.Syntax.SyntaxTree GetSyntaxTreeOfThisClass()
-        {
-            string path = GetPath(Assembly.GetExecutingAssembly());
-
-            //var assemblyDefinition = AssemblyDefinition.ReadAssembly(path);
-
-            //var type = assemblyDefinition.MainModule.GetType($"{nameof(CecilExample)}.{nameof(Program)}");
-
-            //var method = type.Methods.Where(x => x.Name == nameof(Main)).Single();
-
-            var settings = new DecompilerSettings();
-
-            var decompiler = new CSharpDecompiler(path, settings);
-
-            var name = new FullTypeName(GetThisTypeName());
-            var syntaxTree = decompiler.DecompileType(name);
-            return syntaxTree;
-        }
-
-        private static string GetThisTypeName()
-        {
-            return $"{nameof(CecilExample)}.{nameof(Program)}";
-        }
-
-        private static ICSharpCode.Decompiler.CSharp.Syntax.SyntaxTree GetSyntaxTreeOfThisMethod()
-        {
-            string path = GetPath(Assembly.GetExecutingAssembly());
-
-            //var assemblyDefinition = AssemblyDefinition.ReadAssembly(path);
-
-            //var type = assemblyDefinition.MainModule.GetType($"{nameof(CecilExample)}.{nameof(Program)}");
-
-            //var method = type.Methods.Where(x => x.Name == nameof(Main)).Single();
-
-            var settings = new DecompilerSettings();
-
-            var decompiler = new CSharpDecompiler(path, settings);
-
-            var name = new FullTypeName($"{nameof(CecilExample)}.{nameof(Program)}");
-            ITypeDefinition typeInfo = decompiler.TypeSystem.MainModule.Compilation.FindType(name).GetDefinition();
-            var tokenOfFirstMethod = typeInfo.Methods.Where(x => x.Name == nameof(Main)).Single().MetadataToken;
-
-            var syntaxTree = decompiler.Decompile(tokenOfFirstMethod);
-            return syntaxTree;
-        }
-
-        private static string GetPath(Assembly assembly)
+        public static string GetPath(this Assembly assembly)
         {
             string codeBase = assembly.CodeBase;
             UriBuilder uri = new UriBuilder(codeBase);
@@ -126,11 +61,33 @@ namespace CecilExample
             return path;
         }
 
-        private static IEnumerable<string> GetAllReferencesPaths()
+        public static ICSharpCode.Decompiler.CSharp.Syntax.SyntaxTree Decompile(this Assembly assembly, string typeName)
         {
-            foreach (var assemblyName in Assembly.GetExecutingAssembly().GetReferencedAssemblies())
+            string path = GetPath(assembly);
+            var settings = new DecompilerSettings();
+            var decompiler = new CSharpDecompiler(path, settings);
+            var name = new FullTypeName(typeName);
+            var syntaxTree = decompiler.DecompileType(name);
+            return syntaxTree;
+        }
+
+        public static ICSharpCode.Decompiler.CSharp.Syntax.SyntaxTree Decompile(this Assembly assembly, string typeName, string methodName)
+        {
+            string path = GetPath(assembly);
+            var settings = new DecompilerSettings();
+            var decompiler = new CSharpDecompiler(path, settings);
+            var name = new FullTypeName(typeName);
+            ITypeDefinition typeInfo = decompiler.TypeSystem.MainModule.Compilation.FindType(name).GetDefinition();
+            var tokenOfFirstMethod = typeInfo.Methods.Where(x => x.Name == methodName).Single().MetadataToken;
+            var syntaxTree = decompiler.Decompile(tokenOfFirstMethod);
+            return syntaxTree;
+        }
+
+        public static IEnumerable<string> GetAllReferencesPaths(this Assembly assembly)
+        {
+            foreach (var assemblyName in assembly.GetReferencedAssemblies())
             {
-                yield return GetPath(Assembly.Load(assemblyName));
+                yield return Assembly.Load(assemblyName).GetPath();
             }
         }
     }
